0c04eb5da2bd3f4a81eb1c60c4a62384304ee320
Florian Brezina <florian.brezina@outlook.com>
2024-01-27 18:40:48 +0100

    Update server.py for flask rather than pyramid

--- server.py
+++ server.py
@@ -1,20 +1,15 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
 import os
+from flask import Flask
+app = Flask(__name__)
 
-def hello_world(request):
+@app.route('/')
+def hello_world():
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
     message = "Good morning, " + name + "!\n"
-    return Response(message)
+    return message
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    app.run(host='0.0.0.0', port=5000)
