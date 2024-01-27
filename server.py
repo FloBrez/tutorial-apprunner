1693247e7e7f6fce9399e0b1091a06574722eb6f
Florian Brezina <florian.brezina@live.de>
2024-01-27 19:52:14 +0100

    add boto3 access to s3 and ddb

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import os
+import json
+import boto3
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -9,6 +12,27 @@ def hello_world():
         name = "world"
     message = "Good morning with Flask, " + name + "!\n"
     return message
+
+
+@app.route('/s3')
+def test_s3():
+    s3 = boto3.resource('s3')
+    try:
+        message = json.loads(s3.Object('blommberch', 'daten/db-isin-test/AT0000272505.json').get().get('Body').read().decode())
+    except:
+        message = {'Error': 'S3 access failed.'}
+    return message
+
+
+@app.route('/ddb')
+def test_ddb():
+    try:
+        WERTPAPIERE = boto3.resource('dynamodb').Table('Wertpapiere')
+        message = WERTPAPIERE.get_item(Key={'PK': 'ISIN#DE000GP4MFJ1', 'SK': 'KURS#2024-01-12'}).get('Item')
+    except:
+        message = {'Error': 'DynamoDB access failed.'}
+    return message
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT"))
